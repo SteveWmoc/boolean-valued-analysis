0fefdda257eebeee535506b8508bae2dd5309ce9
Steven Sabean <steves1962wmoc@gmail.com>
2026-08-18 20:56:32 -0500

    Unfold ground conjunction wrappers

--- BooleanValuedAnalysis/SetTheory/Ground.lean
+++ BooleanValuedAnalysis/SetTheory/Ground.lean
@@ -182,7 +182,7 @@ theorem groundTruth_inf
     BooleanValued.FirstOrder.BoundedFormula.truth groundStructure.{u}
         (φ ⊓ ψ) assignment boundAssignment ↔ _
   rw [BooleanValued.FirstOrder.BoundedFormula.truth_inf]
-  simp only [inf_Prop_eq]
+  simpa only [groundTruth, inf_Prop_eq]
 
 @[simp]
 theorem groundTruth_all
