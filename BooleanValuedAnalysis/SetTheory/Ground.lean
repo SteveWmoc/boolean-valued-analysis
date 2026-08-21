5ad79e9200a78d04e5aa25d82d5c02aa5382fcf9
Steven Sabean <steves1962wmoc@gmail.com>
2026-08-21 06:50:05 -0500

    Make ground lift simplification cross-version

--- BooleanValuedAnalysis/SetTheory/Ground.lean
+++ BooleanValuedAnalysis/SetTheory/Ground.lean
@@ -106,7 +106,7 @@ theorem groundEvalTerm_liftAt_one_self
   | inl a => rfl
   | inr i =>
       simp only [Function.comp_apply, Sum.map_inr, Sum.elim_inr]
-      simp only [i.isLt]
+      simp [i.isLt]
       have hcast : Fin.castAdd 1 i = Fin.castSucc i := by
         apply Fin.ext
         rfl
